04753d582ff6456c31736fcd8de61f7ac536ec0c
saannsaar <sannsaarinen@gmail.com>
2024-01-03 13:12:59 +0200

    s

--- securityFlawsAppReact/src/services/login.js
+++ securityFlawsAppReact/src/services/login.js
@@ -3,12 +3,15 @@ import axios from 'axios'
 const baseUrl = 'https://security-issues-backened.onrender.com/api/login'
 
 const login = async (cred) => {
-
+ try {
   console.log(document.cookie)
-  const csrfToken = document.cookie
+  const csrfCookie = document.cookie
   .split('; ')
   .find((cookie) => cookie.startsWith('XSRF-TOKEN='))
-  .split('=')[1];
+  if (!csrfCookie) {
+    throw new Error('XSRF-TOKEN cookie not found');
+  }
+  const csrfToken = csrfCookie.split('=')[1];
 
   const response = await axios.post(baseUrl, cred,  {
 
@@ -20,6 +23,11 @@ const login = async (cred) => {
   })
   console.log(response)
   return response.data
+ } catch (error) {
+  console.log(error)
+  throw error
+ }
+ 
 }
 
 export default { login }
